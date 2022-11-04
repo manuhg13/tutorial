3710f677790901ad7d8afa672aea9883165b4ff1
manu <manuhg13@gmail.com>
2022-11-04 17:15:30 +0100

    s

---
+++ Practica9/JS/escuchador.js
@@ -0,0 +1,8 @@
+
+function colorear(){
+    this.style.backgroundColor='lightblue';
+}
+
+Array.from(document.getElementById('idTabla').getElementsByTagName('td')).forEach(element=> {
+    element.addEventListener('dblclick',colorear);
+});
