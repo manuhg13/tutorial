7dcb78ccfcd42243a10aa647d8f866f3fffb5338
manu <manuhg13@gmail.com>
2022-11-04 17:34:47 +0100

    d

--- Practica10/JS/escuchador2.js
+++ Practica10/JS/escuchador2.js
@@ -1,7 +1,9 @@
 let i=1;
 function colorear(){
-    this.style.backgroundColor='lightblue';
-    this.innerHTML+=`-${i++}`;
+    this.style.backgroundColor='lightorange';
+    if (i<=4) {
+        this.innerHTML+=`-${i++}`;
+    }
 }
 
 Array.from(document.getElementById('idTabla').getElementsByTagName('td')).forEach(element=> {
